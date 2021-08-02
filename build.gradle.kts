5aa6c844da74d320e18f8650851566bd7b2453fb
Alexey Lapin <alexey-lapin@protonmail.com>
2021-08-02 20:20:01 +0300

    add github actions support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `java-library`
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -9,6 +11,40 @@ repositories {
 dependencies {
     testImplementation("org.assertj:assertj-core:3.20.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components.getByName("java"))
+        }
+    }
+    repositories {
+        maven {
+            name = "OSSRH"
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = System.getenv("OSSRH_USER") ?: return@credentials
+                password = System.getenv("OSSRH_PASSWORD") ?: return@credentials
+            }
+        }
+    }
+}
+
+configure<SigningExtension> {
+    val key = System.getenv("SIGNING_KEY") ?: return@configure
+    val password = System.getenv("SIGNING_PASSWORD") ?: return@configure
+    val publishing: PublishingExtension by project
+
+    useInMemoryPgpKeys(key, password)
+    sign(publishing.publications)
 }
 
 tasks.test {
